79a0f63d774df453d026bdecc28175715b28d8b5
DanielGSalcedo <134619621+DanielGSalcedo@users.noreply.github.com>
2024-05-29 12:31:21 -0500

    adding search method

--- ImproveCircularSkipList/src/Util/CDSkipList.java
+++ ImproveCircularSkipList/src/Util/CDSkipList.java
@@ -7,7 +7,62 @@ package Util;
 /**
  *
  * @author garci
+ * @param <T>
  */
-public class CDSkipList {
+public class CDSkipList<T extends Comparable> {
     
+    private NodoS<T> centinela;
+    private int size = 0;
+    private int altura = -1;
+
+    public CDSkipList() {
+        this. centinela = new NodoS();
+        this.centinela.setAnterior(centinela);
+        this.centinela.setSiguiente(centinela);
+        this.centinela.setSuperior(centinela);
+        this.centinela.setInferior(centinela);
+        
+    }
+    
+    public NodoS<T> buscar(int key){
+        this.validarKey(key);
+        if(this.estaVacia()) throw new RuntimeException("la lista esta vacia");
+        
+        NodoS<T> aux = this.centinela;
+        if(aux.getInferior().getSiguiente().getKey() < key && aux.getInferior().getAnterior().getKey() < key){
+            
+            while(aux.getInferior() != null){
+                aux = aux.getInferior();
+                while(aux == this.centinela |( aux.getKey()< key && aux.getSiguiente() != this.centinela)){
+                    aux = aux.getSiguiente();
+                    if(aux.getKey() == key ) return aux;
+                }
+            }
+        }else{
+            while(aux.getInferior() != null){
+                aux = aux.getInferior();
+                while(aux == this.centinela | (aux.getKey()> key && aux.getAnterior().getKey()> key && aux.getAnterior()!= this.centinela) ){
+                    aux = aux.getAnterior();
+                    if(aux.getKey() == key ) return aux;
+                    while(aux.getAnterior().getKey() < key && aux.getInferior() != null ){
+                        aux = aux.getInferior();
+                    }
+                }
+            }
+        }
+        if(aux.getKey() > key) return aux.getAnterior();
+        return aux;
+    }
+    
+    public T insertar(T info,int key){
+        
+       return null;
+    }
+    
+    public boolean estaVacia(){
+        return this.size == 0;
+    }
+    private void validarKey(int key){
+        if(key <0 || key > Integer.MAX_VALUE) throw new RuntimeException("key no valida");
+    }
 }
--- ImproveCircularSkipList/src/Util/NodoS.java
+++ ImproveCircularSkipList/src/Util/NodoS.java
@@ -13,19 +13,30 @@ public class NodoS<T> {
     
     private T info;
     
+    private int key;
+    
     private NodoS anterior;
     private NodoS siguiente;
     private NodoS superior;
     private NodoS inferior;
-
-    public NodoS(T info) {
-      this.info = info;
-      this.anterior = this;
-      this.siguiente = this;
-      this.superior = this;
-      this.inferior = this;
+    
+    public NodoS(){
+    
     }
-
+    public NodoS(T info, int key) {
+      this.validarKey(key);
+      this.info = info;
+      this.key = key;
+      this.anterior = null;
+      this.siguiente = null;
+      this.superior = null;
+      this.inferior = null;
+    }
+    
+    private void validarKey(int key){
+        if(key <0 || key > Integer.MAX_VALUE) throw new RuntimeException("key no valida");
+    }
+    
     public T getInfo() {
         return info;
     }
@@ -34,6 +45,14 @@ public class NodoS<T> {
         this.info = info;
     }
 
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+    
     public NodoS getAnterior() {
         return anterior;
     }
